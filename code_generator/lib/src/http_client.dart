8d0dd60e9668d1604ca2b299c293e5525f00e98f
Philip Fryklund <philip.fryklund@alster.se>
2024-05-14 13:00:03 +0200

    Fixed rate limit

--- code_generator/lib/src/http_client.dart
+++ code_generator/lib/src/http_client.dart
@@ -13,6 +13,8 @@ class StoryblokHttpClient {
   final String authorization;
   StoryblokHttpClient(this.spaceId, this.authorization);
 
+  final _rateLimit = _RateLimit();
+
   Future<List<Component>> getComponents() async {
     const key = "components";
     final data = await _get(key);
@@ -42,11 +44,40 @@ class StoryblokHttpClient {
   }
 
   Future<JSONMap> _get(String path, [JSONMap? params]) async {
+    await _rateLimit();
+
     final response = await http.get(
       Uri.https("mapi.storyblok.com", "/v1/spaces/$spaceId/$path", params),
       headers: {"Authorization": authorization},
     );
     final json = jsonDecode(response.body) as JSONMap;
     return json;
+  }
+}
+
+class _RateLimit {
+  static const _throttleLimit = 3;
+
+  DateTime? _currentThrottleSessionStart;
+  int _currentThrottleCounter = 0;
+  Future? _waitFuture;
+
+  void _reset() {
+    _currentThrottleSessionStart = DateTime.now();
+    _currentThrottleCounter = 0;
+    _waitFuture = null;
+  }
+
+  Future<void> call() async {
+    if (_currentThrottleSessionStart == null) {
+      _reset();
+    } else {
+      final timeIntoSession = DateTime.now().difference(_currentThrottleSessionStart!).inMilliseconds;
+      if (_currentThrottleCounter >= _throttleLimit) {
+        _waitFuture ??= Future.delayed(Duration(milliseconds: 1000 - timeIntoSession + 10)).then((_) => _reset());
+        await _waitFuture;
+      }
+    }
+    _currentThrottleCounter += 1;
   }
 }
