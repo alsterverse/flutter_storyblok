6ef46daa2953f519afc1f486c4f09758cb2fb9e5
Tommi Ojaskivi <tommiojaskivi@gmail.com>
2024-05-22 16:19:37 +0200

    Update link and plugin field

--- code_generator/lib/src/fields/plugin_field.dart
+++ code_generator/lib/src/fields/plugin_field.dart
@@ -7,7 +7,11 @@ import '../utils/code_builder_extensions.dart';
 // FieldType enum can not be generated beacuse FieldTypes API returns all available plugins for a user, not the project.
 
 final class PluginField extends BaseField {
-  PluginField.fromJson(super.data) : super.fromJson();
+  PluginField.fromJson(super.data)
+      : fieldType = data["field_type"],
+        super.fromJson();
+
+  final String fieldType;
 
   @override
   late final TypeReference type = referType(
@@ -18,7 +22,7 @@ final class PluginField extends BaseField {
 
   @override
   Expression buildInitializer(CodeExpression valueExpression) {
-    final expression = type.invokeNamed("fromJson", valueExpression);
+    final expression = type.property('fromJson').call([valueExpression, literalString(fieldType)]);
     if (isRequired) return expression;
     return valueExpression.isNotA(refer("$Map")).conditional(literalNull, expression);
   }
