3620a1196170aedd4d8012090cc47e944db1ff9d
Philip Fryklund <philip.fryklund@alster.se>
2024-05-23 10:48:35 +0200

    Skip plugin missing field_type

--- code_generator/lib/src/fields/plugin_field.dart
+++ code_generator/lib/src/fields/plugin_field.dart
@@ -7,7 +7,14 @@ import '../utils/code_builder_extensions.dart';
 // FieldType enum can not be generated beacuse FieldTypes API returns all available plugins for a user, not the project.
 
 final class PluginField extends BaseField {
-  PluginField.fromJson(super.data) : super.fromJson();
+  PluginField.fromJson(super.data)
+      : fieldType = data["field_type"],
+        super.fromJson();
+
+  final String? fieldType;
+
+  @override
+  bool get shouldSkip => fieldType == null;
 
   @override
   late final TypeReference type = referType(
@@ -18,7 +25,7 @@ final class PluginField extends BaseField {
 
   @override
   Expression buildInitializer(CodeExpression valueExpression) {
-    final expression = type.invokeNamed("fromJson", valueExpression);
+    final expression = type.property('fromJson').call([valueExpression, literalString(fieldType!)]);
     if (isRequired) return expression;
     return valueExpression.isNotA(refer("$Map")).conditional(literalNull, expression);
   }
