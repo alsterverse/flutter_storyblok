2d93f909426d01aa8362ff7efd7575a9c97ccb4d
Philip Fryklund <philip.fryklund@alster.se>
2024-05-24 11:00:09 +0200

    Remove Center

--- example/lib/block_widget_builder.dart
+++ example/lib/block_widget_builder.dart
@@ -14,47 +14,42 @@ import 'package:example/components/page.dart' as starter_blocks;
 import 'package:example/components/tabbed_content_entry.dart';
 import 'package:example/components/tabbed_content_section.dart';
 import 'package:example/components/text_section.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 extension BlockWidget on bloks.Blok {
   Widget buildWidget(BuildContext context) {
-    return Center(
-      child: switch (this) {
-        bloks.ArticleOverviewPage blok => ArticleOverviewPageScreen(blok),
-        bloks.ArticlePage blok => ArticlePageScreen(blok),
-        bloks.Author blok => Text(blok.runtimeType.toString()),
-        bloks.Badge blok => Text(blok.runtimeType.toString()),
-        bloks.Banner blok => BannerWidget(blok),
-        bloks.BannerReference blok => BannerReferenceWidget(blok),
-        bloks.Button blok => Text(blok.runtimeType.toString()),
-        bloks.Category blok => CategoryScreen(blok),
-        bloks.ComplexHeroSection blok => Text(blok.runtimeType.toString()),
-        bloks.DefaultPage blok => starter_blocks.Page(page: blok),
-        bloks.FeaturedArticlesSection blok => FeaturedArticlesSectionWidget(blok),
-        bloks.FormSection blok => FormSectionWidget(blok),
-        bloks.GridCard blok => GridCardWidget(blok),
-        bloks.GridSection blok => GridSectionWidget(blok),
-        bloks.HeroSection blok => HeroSectionWidget(blok),
-        bloks.ImageTextSection blok => ImageTextSectionWidget(blok),
-        bloks.Label blok => Text(blok.runtimeType.toString()),
-        bloks.NavItem blok => Text(blok.runtimeType.toString()),
-        bloks.PersonalizedSection blok => Text(blok.runtimeType.toString()),
-        bloks.PriceCard blok => Text(blok.runtimeType.toString()),
-        bloks.RichtextYoutube blok => Text(blok.runtimeType.toString()),
-        bloks.RocketCustomizationPage blok => Text(blok.runtimeType.toString()),
-        bloks.RocketJourneyPage blok => Text(blok.runtimeType.toString()),
-        bloks.RocketJourneyScrollSection blok => Text(blok.runtimeType.toString()),
-        bloks.SingleProductSection blok => Text(blok.runtimeType.toString()),
-        bloks.SiteConfig blok => Text(blok.runtimeType.toString()),
-        bloks.TabbedContentEntry blok => TabbedContentEntryWidget(blok),
-        bloks.TabbedContentSection blok => TabbedContentSectionWidget(blok),
-        bloks.TextSection blok => TextSectionWidget(blok),
-        bloks.TwoColImageTextSection blok => Text(blok.runtimeType.toString()),
-        bloks.UnrecognizedBlok blok => Text(blok.toString()),
-        //TODO: remove this line before release
-        _ => kDebugMode ? const Placeholder() : const SizedBox.shrink(),
-      },
-    );
+    return switch (this) {
+      bloks.ArticleOverviewPage blok => ArticleOverviewPageScreen(blok),
+      bloks.ArticlePage blok => ArticlePageScreen(blok),
+      bloks.Author blok => Text(blok.runtimeType.toString()),
+      bloks.Badge blok => Text(blok.runtimeType.toString()),
+      bloks.Banner blok => BannerWidget(blok),
+      bloks.BannerReference blok => BannerReferenceWidget(blok),
+      bloks.Button blok => Text(blok.runtimeType.toString()),
+      bloks.Category blok => CategoryScreen(blok),
+      bloks.ComplexHeroSection blok => Text(blok.runtimeType.toString()),
+      bloks.DefaultPage blok => starter_blocks.Page(page: blok),
+      bloks.FeaturedArticlesSection blok => FeaturedArticlesSectionWidget(blok),
+      bloks.FormSection blok => FormSectionWidget(blok),
+      bloks.GridCard blok => GridCardWidget(blok),
+      bloks.GridSection blok => GridSectionWidget(blok),
+      bloks.HeroSection blok => HeroSectionWidget(blok),
+      bloks.ImageTextSection blok => ImageTextSectionWidget(blok),
+      bloks.Label blok => Text(blok.runtimeType.toString()),
+      bloks.NavItem blok => Text(blok.runtimeType.toString()),
+      bloks.PersonalizedSection blok => Text(blok.runtimeType.toString()),
+      bloks.PriceCard blok => Text(blok.runtimeType.toString()),
+      bloks.RichtextYoutube blok => Text(blok.runtimeType.toString()),
+      bloks.RocketCustomizationPage blok => Text(blok.runtimeType.toString()),
+      bloks.RocketJourneyPage blok => Text(blok.runtimeType.toString()),
+      bloks.RocketJourneyScrollSection blok => Text(blok.runtimeType.toString()),
+      bloks.SingleProductSection blok => Text(blok.runtimeType.toString()),
+      bloks.SiteConfig blok => Text(blok.runtimeType.toString()),
+      bloks.TabbedContentEntry blok => TabbedContentEntryWidget(blok),
+      bloks.TabbedContentSection blok => TabbedContentSectionWidget(blok),
+      bloks.TextSection blok => TextSectionWidget(blok),
+      bloks.TwoColImageTextSection blok => Text(blok.runtimeType.toString()),
+      bloks.UnrecognizedBlok _ => const Placeholder(),
+    };
   }
 }
